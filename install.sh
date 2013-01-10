01db1340930f695711caa3d0c268f56f24198741
Chirag Khatri <ckhatri4@gmail.com>
2013-01-09 23:24:06 -0500

    Adds gvimrc symbolic link to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ sh
 
 VIMRC="ln -s `pwd`/vimrc .vimrc"
+GVIMRC="ln -s `pwd`/gvimrc .gvimrc"
 VIMDIR="ln -s `pwd`/vim .vim"
 TMUX="ln -s `pwd`/tmux.conf .tmux.conf"
 
@@ -9,6 +10,10 @@ cd
 echo "Linking .vimrc"
 echo $VIMRC
 $VIMRC
+
+echo "Linking .gvimrc"
+echo $GVIMRC
+$GVIMRC
 
 echo "Linking .vim"
 echo $VIMDIR
